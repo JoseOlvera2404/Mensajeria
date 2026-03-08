6f547e54f826fc58bacbab0138c0659204e4f87e
Jose Luis Olvera Garcia <joseolveragarcia24@gmail.com>
2026-03-08 16:32:53 -0600

    Correcion de deploy

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -2,15 +2,26 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+
 dotenv.config();
+
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
+
 app.use(express.json());
+
 app.get("/", (_req, res) => {
-    res.send("API Mensajeria funcionando 🚀");
+  res.send("API Mensajeria funcionando 🚀");
 });
+
 app.use("/api/auth", authRoutes);
+
 const PORT = Number(process.env.PORT) || 4000;
+
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
