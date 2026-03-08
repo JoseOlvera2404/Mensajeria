b5050b445c72f723bf10eecbb05909cefd38b1b7
Jose Luis Olvera Garcia <joseolveragarcia24@gmail.com>
2026-03-08 16:49:16 -0600

    Correcion

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import http from "http";
-import {askAI} from "./services/ai.service.js";
+import pool from "./config/db.js";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -20,7 +20,10 @@ const app = express();
 // Middlewares
 // ============================
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -42,6 +45,10 @@ app.get("/api/health", (req, res) => {
   res.json({ message: "API funcionando correctamente con WebSockets" });
 });
 
+app.get("/db-test", async (_req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json(result.rows);
+});
 
 // ============================
 // HTTP SERVER + SOCKET.IO
